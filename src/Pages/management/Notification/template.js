7aacde67c85fea0a02cc5e73b698113b0fcc8948
Nivedita <nivedita.upadhayay@aayaninfotech.com>
2025-07-18 16:53:46 +0530

    chnages

--- src/Pages/management/Notification/template.js
+++ src/Pages/management/Notification/template.js
@@ -1,4 +1,8 @@
 import motivationalImage from "./images/motivational-reminder1.jpg";
+import motivationalImage1 from "./images/motivational-reminder2.jpg";
+import motivationalImage2 from "./images/motivational-reminder3.jpg";
+import motivationalImage3 from "./images/motivational-reminder4.jpg";
+
 
 export const reminderTemplate1 = `
   <!DOCTYPE html>
@@ -18,3 +22,81 @@ export const reminderTemplate1 = `
     </body>
   </html>
 `;
+
+
+export const reminderTemplate2 = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <title>Email Template</title>
+      <style>
+        body { font-family: Arial, sans-serif; }
+        .container { max-width: 600px; margin: 0 auto; }
+      </style>
+    </head>
+    <body>
+    <div style="background-image: url('${motivationalImage1}');background-size:contain;background-repeat:no-repeat;height:600px">
+        <h1>Hello, Daksh!</h1>
+        <p>Thank you for your interest in our service.</p>
+      </div>
+    </body>
+  </html>
+`;
+
+export const reminderTemplate3 = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <title>Email Template</title>
+      <style>
+        body { font-family: Arial, sans-serif; }
+        .container { max-width: 600px; margin: 0 auto; }
+      </style>
+    </head>
+    <body>
+    <div style="background-image: url('${motivationalImage2}');background-size:contain;background-repeat:no-repeat;height:600px">
+        <h1>Hello, Daksh!</h1>
+        <p>Thank you for your interest in our service.</p>
+      </div>
+    </body>
+  </html>
+`;
+
+export const reminderTemplate4 = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <title>Email Template</title>
+      <style>
+        body { font-family: Arial, sans-serif; }
+        .container { max-width: 600px; margin: 0 auto; }
+      </style>
+    </head>
+    <body>
+    <div style="background-image: url('${motivationalImage3}');background-size:contain;background-repeat:no-repeat;height:600px">
+        <h1>Hello, Daksh!</h1>
+        <p>Thank you for your interest in our service.</p>
+      </div>
+    </body>
+  </html>
+`;
+
+// export const reminderTemplate5 = `
+//   <!DOCTYPE html>
+//   <html>
+//     <head>
+//       <title>Email Template</title>
+//       <style>
+//         body { font-family: Arial, sans-serif; }
+//         .container { max-width: 600px; margin: 0 auto; }
+//       </style>
+//     </head>
+//     <body>
+//     <div style="background-image: url('${motivationalImage3}');background-size:contain;background-repeat:no-repeat;height:600px">
+//         <h1>Hello, Daksh!</h1>
+//         <p>Thank you for your interest in our service.</p>
+//       </div>
+//     </body>
+//   </html>
+// `;
+
