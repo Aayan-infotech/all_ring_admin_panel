db0a9ca720e72fff66332e6775b4a08476d731b6
Nivedita <nivedita.upadhayay@aayaninfotech.com>
2025-09-03 13:43:32 +0530

    ip change

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
 // Create this file to store your API base URL
 // const API_BASE_URL = "http://localhost:5010";
-const API_BASE_URL = "http://98.87.121.113:5010";
+const API_BASE_URL = "http://34.206.193.218:5010"; 
 
 export default API_BASE_URL;
